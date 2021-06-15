59bfd767a1dfb7852f5c05a88c52eedc9b6141c2
Marcelo Ribeiro <marceloribeiromidia@gmail.com>
2021-06-15 18:37:56 -0300

    Refactoring

--- src/smask.js
+++ src/smask.js
@@ -15,10 +15,11 @@ export const mask = (value = "", pattern = "") => {
   if (!value || !pattern) return ""
   const unmasked = [...unmask(pattern)], output = [...unmask(value, pattern)]
   for (let i = 0; i < pattern.length && output[i]; i++)
-    if (!tokens[unmasked[i]].test(output[i])) output.splice(i, 1)
-    else if (/\W/.test(pattern[i]))
-      output.splice(i, 0, pattern[i]) && unmasked.splice(i, 0, pattern[i])
-    else output.splice(i, 1, tokens[unmasked[i]].transform(output[i]))
+    !tokens[unmasked[i]].test(output[i])
+      ? output.splice(i, 1)
+      : /\W/.test(pattern[i])
+      ? output.splice(i, 0, pattern[i]) && unmasked.splice(i, 0, pattern[i])
+      : output.splice(i, 1, tokens[unmasked[i]].transform(output[i]))
   return output.join("")
 }
 
