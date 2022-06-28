80952a72a28b6de557a5b8ed3aafcf8a5894270a
Marcelo Ribeiro <marceloribeiromidia@gmail.com>
2022-06-28 11:50:57 -0300

    Managing packages

--- jest.config.ts
+++ jest.config.ts
@@ -94,6 +94,7 @@ export default {
 
   // A preset that is used as a base for Jest's configuration
   // preset: undefined,
+  preset: "ts-jest",
 
   // Run tests from one or more projects
   // projects: undefined,
@@ -137,7 +138,7 @@ export default {
   // snapshotSerializers: [],
 
   // The test environment that will be used for testing
-  testEnvironment: "jsdom",
+  // testEnvironment: "jsdom",
 
   // Options that will be passed to the testEnvironment
   // testEnvironmentOptions: {},
